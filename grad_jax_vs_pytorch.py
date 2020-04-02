2c0697c0c0f141bde752ab8f434d18c4f47e2a23
Eduardo Pignatelli <edu.pignatelli@gmail.com>
2020-04-02 15:24:49 +0100

    Create grad_jax_vs_pytorch.py

---
+++ grad_jax_vs_pytorch.py
@@ -0,0 +1,17 @@
+import jax
+from jax import lax
+import jax.numpy as np
+import torch
+
+
+def pytorch_test():
+  pass
+  
+def jax_test():
+  pass
+ 
+ @jax.jit
+ def jax_jit_test():
+  pass
+
+  
